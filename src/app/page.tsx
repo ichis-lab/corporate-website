d5cae07d5dcdde71cdc3830b20674639151daa5f
kamite0611 <kamiteakira@gmail.com>
2025-04-03 22:41:09 +0900

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div className="">
+      <Top />
+      <Company />
+    </div>
+  );
 }
+
+const Top = () => {
+  return (
+    <div className="h-[700px] flex items-center bg-amber-200">
+      <div className="container mx-auto">
+        <h2 className="text-4xl font-bold ">OUR MISSION</h2>
+      </div>
+    </div>
+  );
+};
+
+const Company = () => {
+  return (
+    <div>
+      <h2>COMPANY</h2>
+    </div>
+  );
+};
